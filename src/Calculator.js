6f19b790bb20c84d9a0d45e5198e3521f26150e3
Surjoyday Talukdar <surjoydaytalukdar.spn@outlook.com>
2024-04-09 12:58:59 +0530

    Optimised to prevent wasted renders

--- src/Calculator.js
+++ src/Calculator.js
@@ -1,5 +1,5 @@
-import { useState } from 'react';
-import clickSound from './ClickSound.m4a';
+import { memo, useState } from "react";
+import clickSound from "./ClickSound.m4a";
 
 function Calculator({ workouts, allowSound }) {
   const [number, setNumber] = useState(workouts.at(0).numExercises);
@@ -21,8 +21,12 @@ function Calculator({ workouts, allowSound }) {
     <>
       <form>
         <div>
-          <label>Type of workout</label>
-          <select value={number} onChange={(e) => setNumber(+e.target.value)}>
+          <label htmlFor="workout">Type of workout</label>
+          <select
+            id="workout"
+            value={number}
+            onChange={(e) => setNumber(+e.target.value)}
+          >
             {workouts.map((workout) => (
               <option value={workout.numExercises} key={workout.name}>
                 {workout.name} ({workout.numExercises} exercises)
@@ -31,34 +35,37 @@ function Calculator({ workouts, allowSound }) {
           </select>
         </div>
         <div>
-          <label>How many sets?</label>
+          <label htmlFor="sets">How many sets?</label>
           <input
-            type='range'
-            min='1'
-            max='5'
+            id="sets"
+            type="range"
+            min="1"
+            max="5"
             value={sets}
             onChange={(e) => setSets(e.target.value)}
           />
           <span>{sets}</span>
         </div>
         <div>
-          <label>How fast are you?</label>
+          <label htmlFor="fast">How fast are you?</label>
           <input
-            type='range'
-            min='30'
-            max='180'
-            step='30'
+            id="fast"
+            type="range"
+            min="30"
+            max="180"
+            step="30"
             value={speed}
             onChange={(e) => setSpeed(e.target.value)}
           />
           <span>{speed} sec/exercise</span>
         </div>
         <div>
-          <label>Break length</label>
+          <label htmlFor="break">Break length</label>
           <input
-            type='range'
-            min='1'
-            max='10'
+            id="break"
+            type="range"
+            min="1"
+            max="10"
             value={durationBreak}
             onChange={(e) => setDurationBreak(e.target.value)}
           />
@@ -68,8 +75,8 @@ function Calculator({ workouts, allowSound }) {
       <section>
         <button onClick={() => {}}>–</button>
         <p>
-          {mins < 10 && '0'}
-          {mins}:{seconds < 10 && '0'}
+          {mins < 10 && "0"}
+          {mins}:{seconds < 10 && "0"}
           {seconds}
         </p>
         <button onClick={() => {}}>+</button>
@@ -78,4 +85,4 @@ function Calculator({ workouts, allowSound }) {
   );
 }
 
-export default Calculator;
+export default memo(Calculator);
